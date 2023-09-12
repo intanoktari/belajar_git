4818cba771b0547b1c9e4840c38b2b3b5c1ee828
intanoktari <intanoktari2707@gmail.com>
2023-09-12 16:49:02 +0700

    menambahkan file hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+#ini soal kuis :)
+print(((10/2-3)+8**2)/2)
+a=10
+b=2
+c=3
+d=8
+hasil_1 = a / b - c
+hasil_2 = d ** b
+hasil_3 = hasil_1 + hasil_2
+hasil_akhir = hasil_3 / b
+print("hasilnya=" ,  hasil_akhir)
